7835153f75b707c906e3f0e87453bebb8efc3944
Kevin Lang <klang2012@gmail.com>
2016-07-30 11:56:14 -0700

    sessions

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,8 @@
 exports.render = function(req, res) {
+    console.log(req.session.lastVisit || "First visit!");
+
+    req.session.lastVisit = new Date();
+
     res.render('index', {
         title: 'Hello World'
     });
