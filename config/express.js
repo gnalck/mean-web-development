869a88825864da5d90fb1d89a8a4a2ee5fccff8a
Kevin Lang <klang2012@gmail.com>
2016-07-30 11:18:32 -0700

    express middlewear

--- config/express.js
+++ config/express.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var morgan = require('morgan');
+var compress = require('compress');
+var bodyParse = require('body-parser');
+var methodOverride = require('method-override');
 
 module.exports = function() {
     var app = express();
+
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_END === 'production') {
+        app.use(compress());
+    }
+
+    app.use(bodyParse.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParse.json());
+    app.use(methodOverride());
+
     require('../app/routes/index.server.routes.js')(app);
     return app;
 }
