ff31110105a8eb7b5ada178d4e6245f8ec8ab512
Kevin Lang <klang2012@gmail.com>
2016-07-31 11:22:34 -0700

    delete

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -7,7 +7,8 @@ module.exports = function(app) {
 
     app.route('/users/:userid')
         .get(users.read)
-        .put(users.update);
+        .put(users.update)
+        .delete(users.delete);
 
     app.param('userid', users.userById);
 }
