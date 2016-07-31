b812833ef705d24a1278687f0d233426ac707f4f
Kevin Lang <klang2012@gmail.com>
2016-07-30 17:13:08 -0700

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express.js');
+var mongoose = require('./config/mongoose.js');
+
+var db = mongoose();
 
 var app = express();
 app.listen(3000);
