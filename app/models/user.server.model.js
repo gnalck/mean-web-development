e88af66688797219887c75a93943c0367cb9df6c
Kevin Lang <klang2012@gmail.com>
2016-07-31 13:28:09 -0700

    setters/getters/virtuals and URI parsing

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,5 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var URI = require('urijs');
+
+var changeUrl = function(url) {
+    if (!url) {
+        return url;
+    } else {
+        var uri = new URI(url);
+        uri.scheme("http");
+        return uri.toString();
+    }
+};
 
 var UserSchema = new Schema({
     firstName: String,
@@ -13,7 +24,22 @@ var UserSchema = new Schema({
     created: {
         type: Date,
         default: Date.now
+    },
+    website: {
+        type: String,
+        get: changeUrl,
+        set: changeUrl,
     }
 });
+
+UserSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
