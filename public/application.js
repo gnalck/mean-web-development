fe2c43d74262ae0cf95dab09fcb65cca5acd950b
Kevin Lang <klang2012@gmail.com>
2016-08-06 09:54:06 -0700

    angular-route

--- public/application.js
+++ public/application.js
@@ -1,6 +1,13 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, 
+    ['example', 'ngRoute']);
+
+mainApplicationModule.config(['$locationProvider', 
+    function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
+]);
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainApplicationModuleName]);
