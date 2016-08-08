fb2a121e950128c64e140c50677f537077fce7f8
Kevin Lang <klang2012@gmail.com>
2016-08-07 18:59:59 -0700

    articles crud module front end

--- public/application.js
+++ public/application.js
@@ -1,7 +1,7 @@
 var mainApplicationModuleName = 'mean';
 
 var mainApplicationModule = angular.module(mainApplicationModuleName, 
-    ['example', 'ngRoute', 'users']);
+    ['example', 'ngRoute', 'ngResource', 'users', 'articles']);
 
 mainApplicationModule.config(['$locationProvider', 
     function($locationProvider) {
